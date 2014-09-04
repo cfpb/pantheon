10a8492c5386550023691258b8bf726889793db0
David Greisen <david.greisen@cfpb.gov>
2014-09-04 16:47:06 -0400

    document appring

--- devops/appring.py
+++ devops/appring.py
@@ -10,48 +10,15 @@ from django.conf import settings
 from importlib import import_module
 from types import ModuleType
 
-class Models(object):
-    def __init__(self):
-        self.cache = apps
-
-    def __getattr__(self, name):
-        """
-        If the package doesn't exist, then locate it, attach it and return it
-        """
-        try:
-            package = ModelPackage(self.cache.get_app(name), name, self)
-            setattr(self, name, package)
-            return package
-        except:
-            raise AttributeError("object 'Models' has no attribute '{}'".format(name))
-
-    def _get_models(self, pkg):
-        """
-        get the models for a given package. used by Package children
-        """
-        return self.cache.get_models(pkg)
-
-
-class ModelPackage(object):
-    def __init__(self, package, name, parent):
-        self.parent = parent
-        self.package = package
-        self.name = name
-
-    def __getattr__(self, name):
-        try:
-            if not hasattr(self, 'models'):
-                models = self.parent._get_models(self.package)
-                self.models = {x.__name__: x for x in models}
-            return self.models[name]
-        except:
-            raise AttributeError("module '{}' has no attribute {}".format(self.name, name))
-
-
 class Apps(object):
+    """
+    Return a ModuleWrapper-wrapped instance of the app module. The ModuleWrapper
+    makes it easy to get to nested submodules through a dot notation. No need to
+    do further importing.
+    """
     def __init__(self):
         self.app_paths = {x.split('.')[-1]: x for x in settings.INSTALLED_APPS}
-       
+
     def __getattr__(self, name):
         """
         If the package doesn't exist, then locate it, attach it and return it
@@ -98,5 +65,4 @@ class ModuleWrapper(object):
         return repr(module)
 
 
-models = Models()
 apps = Apps()
