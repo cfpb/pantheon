669b518119cf46acab3a46d9c9710ba5237144ac
David Greisen <david.greisen@cfpb.gov>
2014-09-11 13:49:03 -0400

    tabs; alerts; moved refresh
    each repo action now has its own tab. tabs don't work properly -
    all except first have to be clicked to show anything - but think
    this is an upstream javascript bug.
    
    alerts now use the cap framework alerts styling and markup.
    
    moved refresh button to core. this is logical, now that there is
    a global, pluggable sync mechanism handled by core. Also a
    necessary step for splitting GH and GHE into two different tiles.

--- devops/github/views.py
+++ devops/github/views.py
@@ -1,21 +1,7 @@
-from django.contrib import messages
-from django.http import HttpResponseNotAllowed, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
-from github.models import Repo
 from django.template import RequestContext
 from github import forms
-
-# Create your views here.
-def refresh(request):
-    if request.method != 'POST':
-        return HttpResponseNotAllowed()
-    elif request.user.is_authenticated:
-        Repo.objects.sync_user(request.user)
-        messages.success(request, 'Repositories refreshed')
-    else:
-        messages.error(request, 'You must login')
-    return HttpResponseRedirect('/')
-
 
 def enterprise_details(request):
     context = RequestContext(request)
