38e9fa442464cdacc724dc2b7408d8e5b4d2cc6d
David Greisen <david.greisen@cfpb.gov>
2014-11-13 11:30:45 -0500

    removed 2fa org; copy pub key option; better error handling
    we now properly handle errors in github details form;
    
    removed 2fa org. just too complicated. Now make you affirm you
    are using 2fa.
    
    we now copy your ghe pub key over to gh.

--- devops/osw/forms.py
+++ devops/osw/forms.py
@@ -4,5 +4,20 @@ class NameForm(forms.Form):
     update_public_name = forms.BooleanField(required=False)
     updated_name = forms.CharField(max_length=100, required=False)
 
+    def clean(self):
+        data = self.cleaned_data
+        if data.get('update_public_name') and not data.get('updated_name'):
+            raise forms.ValidationError("'Updated Name' is required if you want to update your public name.")
+
 class PublicizeForm(forms.Form):
-    make_membership_public = forms.BooleanField(required=False, help_text="Become a proud public member of our org.")
+    make_membership_public = forms.BooleanField(required=False, help_text='Become a proud public member of our org.')
+
+class PubKeyForm(forms.Form):
+    add_public_key = forms.BooleanField(required=False, help_text='Do you want to add the following GitHub Enterprise key to your public GitHub account?')
+    key_to_add = forms.ChoiceField(choices=[], required=False)
+    key_name = forms.CharField(max_length=20, required=False)
+
+    def clean(self):
+        data = self.cleaned_data
+        if data.get('add_public_key') and (not data.get('key_to_add') or not data.get('key_name')):
+            raise forms.ValidationError("'Key To Add' and 'Key Name' are required if you want to add a public key.")
