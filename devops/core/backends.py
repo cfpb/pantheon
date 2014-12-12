d6215b86388178d44c87470f4135b1a84706a4d7
David Greisen <david.greisen@cfpb.gov>
2014-12-12 12:27:48 -0500

    collect gh/ghe username in addition to id

--- devops/core/backends.py
+++ devops/core/backends.py
@@ -2,6 +2,11 @@ from social.backends.github import GithubOAuth2
 from django.conf import settings
 
 class GitHub(GithubOAuth2):
+    EXTRA_DATA = [
+            ('id', 'id'),
+            ('expires', 'expires'),
+            ('username', 'username'),
+        ]
     def get_user_details(self, response):
         out = super(GitHub, self).get_user_details(response)
         out['gh_id'] = response.get('id')
@@ -12,6 +17,11 @@ class GitHubEnterprise(GithubOAuth2):
     name = 'github-enterprise'
     AUTHORIZATION_URL = settings.SOCIAL_AUTH_GITHUB_ENTERPRISE_HOST + '/login/oauth/authorize'
     ACCESS_TOKEN_URL = settings.SOCIAL_AUTH_GITHUB_ENTERPRISE_HOST + '/login/oauth/access_token'
+    EXTRA_DATA = [
+            ('id', 'id'),
+            ('expires', 'expires'),
+            ('username', 'username'),
+        ]
 
     def _user_data(self, access_token, path=None):
         url = self.setting('HOST') + '/api/v3/user{0}'.format(path or '')
